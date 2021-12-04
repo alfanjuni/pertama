2721a45643d9f04e32f7632524f7ba62b69be4d4
KODINGSQUAD <alfanjuni@gmail.com>
2021-12-04 11:19:55 +0700

    array

--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("aku sayang golang")
+	// fmt.Print("aku sayang golang")
+
+	//ARRAY
+	var languages [5]string
+	languages[0] = "Go"
+	languages[1] = "javascript"
+	languages[2] = "ruby"
+	languages[3] = "php"
+	languages[4] = "java"
+	fmt.Println(languages)
 }
