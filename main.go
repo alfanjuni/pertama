be9d9413dd1f2504577ddf8448dd081ca2947a4d
KODINGSQUAD <alfanjuni@gmail.com>
2021-12-04 11:26:25 +0700

    slice

--- main.go
+++ main.go
@@ -6,17 +6,26 @@ func main() {
 	// fmt.Print("aku sayang golang")
 
 	//ARRAY
-	var languages [5]string
-	languages[0] = "Go"
-	languages[1] = "javascript"
-	languages[2] = "ruby"
-	languages[3] = "php"
-	languages[4] = "java"
+	// var languages [5]string
+	// languages[0] = "Go"
+	// languages[1] = "javascript"
+	// languages[2] = "ruby"
+	// languages[3] = "php"
+	// languages[4] = "java"
 
-	languages2 := [5]string{"Ruby", "GO", "javascript", "java", "C"}
-	fmt.Println(languages2)
+	// languages2 := [5]string{"Ruby", "GO", "javascript", "java", "C"}
+	// fmt.Println(languages2)
 
-	for index, lang := range languages {
-		fmt.Println("index:", index, "languange:", lang)
-	}
+	// for index, lang := range languages {
+	// 	fmt.Println("index:", index, "languange:", lang)
+	// }
+
+	//SLICE
+	var gamingConsoles []string
+
+	gamingConsoles = append(gamingConsoles, "PlayStation4")
+	gamingConsoles = append(gamingConsoles, "xbox1")
+
+	fmt.Println(gamingConsoles)
+
 }
